c1aa6d79c5dd1ec29b18accc74f3d67d1ca302df
vie abie <ochiaabe@gmail.com>
2025-09-18 19:02:41 +0800

    Add files via upload

---
+++ Node.java
@@ -0,0 +1,26 @@
+
+package node;
+
+ class Node {
+     
+     int data;
+     Node left;
+     Node right;
+     
+    public Node(int data){
+        
+        this.data = data;
+     
+ }
+    class BinaryTree{
+        
+        Node root;
+        
+        public void inorder(node n, ist)
+    }
+
+    public static void main(String[] args) {
+       
+    }
+    
+}
